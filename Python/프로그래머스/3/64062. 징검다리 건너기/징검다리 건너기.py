0d7d09df3990fbb41505efce454d7bd3312db451
Beomgeun Choi <qjarmsdl9841@gmail.com>
2026-09-15 00:47:25 +0900

    [level 3] Title: 징검다리 건너기, Time: 57.60 ms, Memory: 19.7 MB -BaekjoonHub

--- Python/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ Python/프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -5,6 +5,7 @@ def solution(stones, k):
     dq = deque()
     for ind, item in enumerate(stones):
         if ind < k:
+            # 값(item)을 넣는 알고리즘
             while dq:
                 temp = dq[-1]
                 if stones[temp] > item:
@@ -13,10 +14,11 @@ def solution(stones, k):
                     dq.pop()
             dq.append(ind)
             #print(f"now index: {ind}, numdq: {list(map(lambda x: stones[x], dq))}")
-        else:
-            temp = dq.popleft()
-            if temp > ind - k:
-                dq.appendleft(temp)
+        else: # 가장 앞 인덱스를 확인하고 슬라이드에서 벗어난 범위면 제거하는 과정을 앞에 추가
+            temp = dq[0]
+            if temp <= ind - k:
+                dq.popleft()
+            # 값(item)을 넣는 알고리즘
             while dq:
                 temp = dq[-1]
                 if stones[temp] > item:
@@ -25,9 +27,8 @@ def solution(stones, k):
                     dq.pop()
             dq.append(ind)
             #print(f"now index: {ind}, numdq: {list(map(lambda x: stones[x], dq))}")
-        if ind >= k - 1:
-            temp = dq.popleft()
+        if ind >= k - 1: # 윈도우가 완성됨 따라서 정답과 비교 후 업데이트 진행
+            temp = dq[0]
             if answer > stones[temp]:
                 answer = stones[temp]
-            dq.appendleft(temp)
     return answer
